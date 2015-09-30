59272f1bf66f5e387a4186e59f090585e091eb5b
PARAPLU <anton.patokin@student.kdg.be>
2015-09-30 11:13:51 +0200

    Oplossing toegevoegd verbeter

---
+++ opdracht-verbetering/oplosing-verbeter.php
@@ -0,0 +1,21 @@
+<?php
+	$tekst 			= 	'Test 123...';
+	$tekst2			= 	'Test 456';
+	$tekst3 		= 	'test';
+	$zin 			= 	"Jean D\'Haeseleir gaat naar Praag!!";
+	$beschrijving 	= 	"Vogelbekdier";
+
+?>
+
+<!DOCTYPE html>
+<mtml>
+<head></head>
+<body>
+
+	<h1>Foutjes verbeteren</h1>
+<?php 
+	echo "werkt </br>";
+	echo "werkt".$tekst." ".$tekst2." ".$tekst3." ".$zin." ".$beschrijving;
+?>
+</body>
+</html>
